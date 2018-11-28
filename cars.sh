9be0d168d7c1d5cf7af91e42f8a7473a7ae53494
Gregory Albarian <albarian@chapman.edu>
2018-11-28 12:50:28 -0800

    Working cars.sh

--- cars.sh
+++ cars.sh
@@ -2,23 +2,40 @@
 #cars.sh
 #by Gregory Albarian
 
-$MENU_CHOICE = 3
+MENU_CHOICE=3
 
-while [ $MENU_CHOICE -eq  "q" ]; do 
-	echo "Menu"
+while [ "$MENU_CHOICE" !=  "q" ]; do 
+	
+	echo "Menu:"
 	echo "a) Add a car"
 	echo "v) View list of cars"
-	echo "q) Quit"
+	echo -e "q) Quit \n"
 	
 	echo "Choose an option: "
 	read MENU_CHOICE
 	
 	case "$MENU_CHOICE" in
+	
 	"a")
-		;;
-	"v") sort My_old_cars | tr ";" " ";;
-	"q") echo "Goodbye"
-;;
-	*) echo "ERROR";;
+		echo "What is the year of the car? "
+		read YEAR
+		echo "What is the make of the car? "
+		read MAKE
+		echo "What is the model of the car? "
+		read MODEL
+			
+		echo "$YEAR:$MAKE:$MODEL" >> My_old_cars;;
+	
+	"v")
+		echo
+		sort My_old_cars | tr ":" " "
+		echo ;;
+	
+	"q")
+		 echo "Goodbye";;
+	
+	*)
+		 echo -e "ERROR\n";;
+	
 	esac
 done
